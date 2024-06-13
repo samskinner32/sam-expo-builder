59061ba0d19909950ba3caa9100912c12f8e568a
Sam Skinner <samskinner@sams-mbp.mynetworksettings.com>
2024-06-13 11:15:06 -0400

    adding builder

--- app/(tabs)/(shop)/index.tsx
+++ app/(tabs)/(shop)/index.tsx
@@ -1,9 +1,33 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 
+import {
+  Content,
+  fetchOneEntry,
+  type BuilderContent,
+} from "@builder.io/sdk-react-native";
 import { Link, router } from "expo-router";
 
+const BUILDER_API_KEY = "<key>";
+const MODEL_NAME = "page";
+
 const Shop = () => {
+  const [content, setContent] = React.useState<BuilderContent | null>(null);
+
+  React.useEffect(() => {
+    fetchOneEntry({
+      model: MODEL_NAME,
+      apiKey: BUILDER_API_KEY,
+      userAttributes: {
+        urlPath: "/",
+      },
+    })
+      .then((data) => {
+        setContent(data);
+      })
+      .catch((err) => console.error("Error fetching Builder Content: ", err));
+  }, []);
+
   const onPress = () => {
     router.navigate("/(shop)/category");
   };
@@ -11,11 +35,19 @@ const Shop = () => {
   return (
     <View>
       <Text>Shop</Text>
-      <Link href="./category">Relative Category</Link>
-      <Link href="/(shop)/category">Absolute Category</Link>
+      <Link href="/(shop)/category">Category</Link>
       <TouchableOpacity onPress={onPress}>
         <Text>Link to category</Text>
       </TouchableOpacity>
+      {content ? (
+        <Content
+          apiKey={BUILDER_API_KEY}
+          model={MODEL_NAME}
+          content={content}
+        />
+      ) : (
+        <Text>Not Found.</Text>
+      )}
     </View>
   );
 };
